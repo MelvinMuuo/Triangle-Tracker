89e749b14184dbc7c216676bbbf09a401c0c3289
MelvinMuuo <muuomelvin73@gmail.com>
2019-03-25 08:37:46 +0300

    change indentation of triangle.js

--- js/triangle.js
+++ js/triangle.js
@@ -3,13 +3,17 @@ function power(){
   var side1=parseFloat(document.getElementById)("side2").value;
   var side1=parseFloat(document.getElementById)("side3").value;
 
-  if(side1===side2 && side2===side3) {
-    output.innerHTML=("This is an equilateral triangle.")
-  } else if (side1===side2 || side2===side3 || side1===side3) {
-      output.innerHTML=("This is an isosceles triangle.")
-  }else if (side1!==side2!==side3 && side1+side2>side3 || side2+side3>side1 || side1+side3>side2) {
-      output.innerHTML=("This is an scalene triangle.")
-  } else {
-      output.innerHTML=("This is not a triangle.")
-  }
+if(side1===side2 && side2===side3) {
+  alert("This is an equilateral triangle.");
+} else if (side1===side2 || side2===side3 || side1===side3) {
+  alert("This is an isosceles triangle.");
+}else if (side1!==side2!==side3 && side1+side2>side3 || side2+side3>side1 || side1+side3>side2) {
+  alert("This is an scalene triangle.");
+} else {
+  alert("This is not a triangle.");
+}
+}
+
+function resetform() {
+document.getElementById("myform").reset();
 }
